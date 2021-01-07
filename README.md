e6351d8860ab12ff417d13771ac9878b497e2cdb
Pierre Carru <pierre.carru@gmail.com>
2021-01-07 19:14:54 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+go get -v -u -tags=gles2,wayland github.com/go-gl/glfw/v3.3/glfw
+go get -v -u -tags=gles2,wayland github.com/go-gl/gl/v3.1/gles2
+go get -v -u -tags=gles2,wayland github.com/go-gl/mathgl/mgl32
+go run -v -tags=gles2,wayland a.go
+```
---
+++ a.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+
+	gl "github.com/go-gl/gl/v3.1/gles2"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+var vshader = `
+varying vec4 a_position;
+
+void main() {
+	gl_Position = a_position;
+}
+`
+var fshader = `
+void main() {
+	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+`
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("OpenGL version", gl.GoStr(gl.GetString(gl.VERSION)))
+	fmt.Println("OpenGL renderer", gl.GoStr(gl.GetString(gl.RENDERER)))
+
+	// Configure the vertex and fragment shaders
+	program, err := newProgram(vshader, fshader)
+	if err != nil {
+		panic(err)
+	}
+
+	gl.ClearColor(1., 0., 0., 0.5)
+
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		gl.UseProgram(program)
+		// window.r
+
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
